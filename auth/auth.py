607f6645e99a972238c96658ec3ef946fab01538
sspring963 <sspring963@gmail.com>
2020-12-13 22:44:27 -0600

    original variables

--- auth/auth.py
+++ auth/auth.py
@@ -6,9 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
-ALGORITHMS = os.environ['ALGORITHMS']
-API_AUDIENCE = os.environ['API_AUDIENCE']
+AUTH0_DOMAIN = 'dev-lyyzp66h.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'movie'
 
 # AuthError Exception
 '''
